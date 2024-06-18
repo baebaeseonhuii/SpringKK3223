01b31e44449f98edc8379c0b2e76838d055e1d47
baebaeseonhuii <qotjsgml3@naver.com>
2024-06-18 16:16:32 +0900

    weather list

--- src/main/java/com/kk3223/app/weather/WeatherController.java
+++ src/main/java/com/kk3223/app/weather/WeatherController.java
@@ -1,5 +1,9 @@
 package com.kk3223.app.weather;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +15,11 @@ public class WeatherController {
 	private WeatherService weatherService;
 
 	@RequestMapping("/weather/list")
-	public String getList() {
-		System.out.println("list 가져오기");
-		weatherService.getList();
+	public String getList(HttpServletRequest request) throws Exception {
 
+		System.out.println("list 가져오기");
+		List<WeatherDTO> weatherList = weatherService.getList();
+		request.setAttribute("weatherList", weatherList);
 		return "weather/list";
 	}
 
--- src/main/java/com/kk3223/app/weather/WeatherDAO.java
+++ src/main/java/com/kk3223/app/weather/WeatherDAO.java
@@ -1,12 +1,46 @@
 package com.kk3223.app.weather;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class WeatherDAO {
 
-	public void getList() {
+	public List<WeatherDTO> getList() throws Exception {
 		System.out.println("DAO list");
+		File file = new File("C:\\study\\weather.txt");
+		FileReader fr = new FileReader(file); // 읽는 객체
+		BufferedReader br = new BufferedReader(fr); // 연결해주는 애
+
+		ArrayList<WeatherDTO> ar = new ArrayList<WeatherDTO>();
+		while (true) {
+			String s = br.readLine();// 한줄씩 읽는 애
+
+			if (s == null) {
+				break;
+			}
+			s = s.replace(",", "-");
+			StringTokenizer st = new StringTokenizer(s, "-");
+			WeatherDTO dto = new WeatherDTO();
+
+			while (st.hasMoreTokens()) {
+				dto.setNum(Long.parseLong(st.nextToken().trim()));
+				dto.setCity(st.nextToken().trim());
+				dto.setGion(Double.parseDouble(st.nextToken().trim()));
+				dto.setStatus(st.nextToken().trim());
+				dto.setHumidity(Integer.parseInt(st.nextToken().trim()));
+			}
+			ar.add(dto);
+		}
+		br.close();
+		fr.close();
+		return ar;
 	}
 
 }
