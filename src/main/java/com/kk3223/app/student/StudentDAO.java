8a4312fbb15778627fce0cd900b81a33e359daf5
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-06-18 18:20:44 +0900

    feat: Modify FileIO -> BufferedIO (utf-8)

--- src/main/java/com/kk3223/app/student/StudentDAO.java
+++ src/main/java/com/kk3223/app/student/StudentDAO.java
@@ -1,9 +1,14 @@
 package com.kk3223.app.student;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,17 +22,16 @@ public class StudentDAO {
 	public StudentDAO() {
 		this.file = new File("C://study//student.txt");
 	}
-	
+
 	public List<StudentDTO> getStudents() throws Exception {
-		FileReader fr = new FileReader(file);
-		BufferedReader br = new BufferedReader(fr);
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
 		String info = br.readLine();
 		String[] infos = null;
 		List<StudentDTO> sDTOs = new ArrayList<StudentDTO>();
 
 		while (info != null) {
 			infos = info.split(",");
-			if(info.length() < 5) break;
+			if (info.length() < 5) break;
 			StudentDTO sDTO = new StudentDTO();
 
 			sDTO.setNum(Long.parseLong(infos[0].trim()));
@@ -44,22 +48,21 @@ public class StudentDAO {
 		}
 
 		br.close();
-		fr.close();
 		return sDTOs;
 	}
-	
+
 	public void makeStudent(StudentDTO sDTO) throws Exception {
-		FileWriter fw = new FileWriter(file, true);
+		BufferedWriter fw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
 		StringBuffer sb = new StringBuffer();
 		int total = sDTO.getKor() + sDTO.getEng() + sDTO.getMath();
 		double avg = total / 3.0;
-		sb.append(sDTO.getNum()).append(",").append(sDTO.getName()).append(",")
-		.append(sDTO.getKor()).append(",").append(sDTO.getEng()).append(",").append(sDTO.getMath()).append(",")
-		.append(total).append(",").append(avg).append("\n");
+		sb.append(sDTO.getNum()).append(",").append(sDTO.getName()).append(",").append(sDTO.getKor()).append(",")
+				.append(sDTO.getEng()).append(",").append(sDTO.getMath()).append(",").append(total).append(",")
+				.append(avg).append("\n");
 		fw.write(sb.toString());
 		fw.close();
 	}
-	
+
 	public StudentDTO getDetail(StudentDTO sDTO) throws Exception {
 		List<StudentDTO> sDTOs = this.getStudents();
 		for (StudentDTO sDTOTmp : sDTOs) {
@@ -70,39 +73,40 @@ public class StudentDAO {
 		}
 		return null;
 	}
-	
+
 	public void delete(StudentDTO sDTO) throws Exception {
 		List<StudentDTO> sDTOs = this.getStudents();
-		FileWriter fw = new FileWriter(file,false);
+		FileWriter fw = new FileWriter(file, false);
 		StringBuffer sb = new StringBuffer();
-		for(StudentDTO sDTOtmp : sDTOs) {
-			if(sDTOtmp.getNum() == sDTO.getNum()) {
+		for (StudentDTO sDTOtmp : sDTOs) {
+			if (sDTOtmp.getNum() == sDTO.getNum()) {
 				continue;
 			}
-			sb.append(sDTOtmp.getNum()).append(",").append(sDTOtmp.getName()).append(",")
-			.append(sDTOtmp.getKor()).append(",").append(sDTOtmp.getEng()).append(",").append(sDTOtmp.getMath()).append(",")
-			.append(sDTOtmp.getTotal()).append(",").append(sDTOtmp.getAvg()).append("\n");
+			sb.append(sDTOtmp.getNum()).append(",").append(sDTOtmp.getName()).append(",").append(sDTOtmp.getKor())
+					.append(",").append(sDTOtmp.getEng()).append(",").append(sDTOtmp.getMath()).append(",")
+					.append(sDTOtmp.getTotal()).append(",").append(sDTOtmp.getAvg()).append("\n");
 		}
 		fw.write(sb.toString());
 		fw.close();
 	}
-	
+
 	public void update(StudentDTO sDTO) throws Exception {
 		List<StudentDTO> sDTOs = this.getStudents();
-		FileWriter fw = new FileWriter(file,false);
+		BufferedWriter fw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
 		int total = sDTO.getKor() + sDTO.getEng() + sDTO.getMath();
 		double avg = total / 3.0;
 		sDTO.setTotal(total);
 		sDTO.setAvg(avg);
 		StringBuffer sb = new StringBuffer();
-		for(int i = 0; i < sDTOs.size(); i++) {
-			if(sDTO.getNum() == sDTOs.get(i).getNum()) {
+		for (int i = 0; i < sDTOs.size(); i++) {
+			if (sDTO.getNum() == sDTOs.get(i).getNum()) {
 				sDTOs.set(i, sDTO);
 			}
 			sb.append(sDTOs.get(i).getNum()).append(",").append(sDTOs.get(i).getName()).append(",")
-			.append(sDTOs.get(i).getKor()).append(",").append(sDTOs.get(i).getEng()).append(",").append(sDTOs.get(i).getMath()).append(",")
-			.append(sDTOs.get(i).getTotal()).append(",").append(sDTOs.get(i).getAvg()).append("\n");
-		
+					.append(sDTOs.get(i).getKor()).append(",").append(sDTOs.get(i).getEng()).append(",")
+					.append(sDTOs.get(i).getMath()).append(",").append(sDTOs.get(i).getTotal()).append(",")
+					.append(sDTOs.get(i).getAvg()).append("\n");
+
 		}
 		fw.write(sb.toString());
 		fw.close();
